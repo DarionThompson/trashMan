99a06c6bc49cd55ed84aef8877850b592fa30c1f
Darion Thompson <darionthompson2@gmail.com>
2021-05-19 14:55:48 -0400

    initial commit

---
+++ Trash/Trash/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Trash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
